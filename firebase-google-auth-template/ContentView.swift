b10da411c60c29a6fe0e29adee57c7d3c191a135
Min Pan <minp4545@gmail.com>
2024-11-18 01:33:55 -0500

    Set up xcode development

---
+++ firebase-google-auth-template/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  firebase-google-auth-template
+//
+//  Created by Min Pan Developer on 11/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ firebase-google-auth-template/firebase_google_auth_templateApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebase_google_auth_templateApp.swift
+//  firebase-google-auth-template
+//
+//  Created by Min Pan Developer on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct firebase_google_auth_templateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
